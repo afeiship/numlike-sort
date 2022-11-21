1216acb1cbd434548523365434564a08a4270169
afeiship <1290657123@qq.com>
2022-11-21 19:16:27 +0800

    refactor: upt var name

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,21 @@
 type Iterator = (item: string, index: number) => string;
+type RealIterator = (item: string, index: number) => [string, number];
 
 const NOT_NUM_RE = /\D/g;
+const EMP = ''
 
-export default (list: string[], inIterator?: Iterator): string[] => {
-  const iterator = inIterator || ((item: string) => item.replace(NOT_NUM_RE, ''));
-  const realIterator = (item: string, index: number): [string, number] => [iterator(item, index), index];
-  const preprocessedList = list.map(realIterator);
+export default (inArray: string[], inIterator?: Iterator): string[] => {
+  const iterator = inIterator || ((item: string) => item.replace(NOT_NUM_RE, EMP));
+  const realIterator: RealIterator = (item, index) => [iterator(item, index), index];
+  const preprocessedList = inArray.map(realIterator);
   const result = preprocessedList.sort((a, b) => {
     const aNum = Number(a[0]);
     const bNum = Number(b[0]);
 
     if (aNum === bNum) return 0;
 
-    return Number(aNum) > Number(bNum) ? 1 : -1;
+    return aNum > bNum ? 1 : -1;
   });
 
-  return result.map((item) => list[item[1]]);
+  return result.map((item) => inArray[item[1]]);
 };
